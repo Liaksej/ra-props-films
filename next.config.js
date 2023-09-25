1f92af64791b57acfda931c29fc23034582ff552
Liaksej <tarusov.liaksej@gmail.com>
2023-09-25 14:06:33 +0200

    Change base path for production environment in next.config.js
    In next.config.js, the base path for the production environment was updated. Previously, it was set to "/ra-event-state-dropdown", which did not match with the current project "/ra-props-film". This change ensures that the correct base path is used when the NODE_ENV is set to production.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath:
-    process.env.NODE_ENV === "production" ? "/ra-event-state-dropdown" : "",
+  basePath: process.env.NODE_ENV === "production" ? "/ra-props-film" : "",
 };
 
 module.exports = nextConfig;
